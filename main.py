47e81126756c3c816a9e16e72f155126adfcd781
eber-antonio <eber.antonio9298@gmail.com>
2025-09-05 20:08:09 -0600

    Esqueleto del programa

---
+++ main.py
@@ -0,0 +1,30 @@
+import argparse
+
+def get_arguments():
+    parser = argparse.ArgumentParser(description="Task Tracker for CLI")
+    parser.add_argument('add')
+
+class Expense:
+    def __init__():
+        pass
+
+    def add_expense(expense, description):
+        pass
+
+    def update_expense():
+        pass
+
+    def delete_expense():
+        pass
+
+    def view_expenses():
+        pass
+
+    def view_summary():
+        pass
+
+    def view_summary_by_month():
+        pass
+
+def main():
+    pass
